b0cab823cceeab1db74a32023c4a013875ce16cd
petrikuittinen <petri.kuittinen@hamk.fi>
2025-04-23 18:21:51 +0300

    guestbook tutorial completed

--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -29,4 +29,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('hello/', include('hello.urls')),
     path('dictionary/', include('dictionary.urls')),
+    path('guestbook/', include('guestbook.urls')),    
 ]
